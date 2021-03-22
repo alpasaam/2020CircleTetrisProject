8a8016a5eed2474b6bff0caae2904aa72bb1d5dd
alpasaam <alpasaam422@gmail.com>
2021-03-22 12:01:41 -0600

    main shapes

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,11 @@
 public class Menu {
-  
+  // This is what the user will see first when they open the program.
+
+  // Title
+
+  // Start button
+
+  // High Scores
+
+  //
 }
--- src/Shapes.java
+++ src/Shapes.java
@@ -1,3 +1,18 @@
 public class Shapes {
+  // In this class we will be storing all the shapes that will be in the game
+
+  // SShape
+
+  // SqShape
   
+  // LShape
+
+  // MLShape
+
+  // TShape
+
+  // ZShape
+
+  // LiShape
+
 }
